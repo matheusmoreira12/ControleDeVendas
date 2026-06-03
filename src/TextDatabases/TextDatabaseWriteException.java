d3b2e2d83f6c5902f55b42459c0f1cadd5ee8811
matheusmoreira12 <matheusmatos32@gmail.com>
2026-06-03 15:19:18 -0300

    More exception overloads

--- src/TextDatabases/TextDatabaseWriteException.java
+++ src/TextDatabases/TextDatabaseWriteException.java
@@ -1,7 +1,17 @@
 package TextDatabases;
 
 public class TextDatabaseWriteException extends Throwable {
+    private static final String DEFAULT_MESSAGE = "A leitura do banco de dados em texto falhou.";
+
     public TextDatabaseWriteException(String message, Throwable cause) {
         super(message, cause);
+    }
+
+    public TextDatabaseWriteException(Throwable cause) {
+        super(DEFAULT_MESSAGE, cause);
+    }
+
+    public TextDatabaseWriteException() {
+        super(DEFAULT_MESSAGE, null);
     }
 }
